07be1c3adb9f8c58f82e829658549e5d4db23562
Rakshitha Acharya <rakshitha@example.com>
2025-08-02 19:55:07 +0530

    Initial commit: Added C++ files and config

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout<<"Enther value for A:";
+    cin>>a;
+    cout<<"Entered number is"<<a;
+    cout << "Hello from MSYS2!" << endl;
+    return 0;
+}
