350f439b4403d20dabaebe2ab5bc3598d553f398
Rakshitha Acharya <rakshitha@example.com>
2025-08-03 18:51:18 +0530

    Add two numbers

---
+++ PROGRAM2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sum, num1, num2;
+    cout<<"Enter the first number : ";
+    cin>>num1;
+    cout<<"\nEnter the second number : ";
+    cin>>num2;4
+    sum = num1 + num2;
+    cout<<"\nAddition two numbers is :"<<sum;
+    return 0;
+}
+
+
